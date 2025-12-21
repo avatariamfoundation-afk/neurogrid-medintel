0a70964637cd9fa4235366ae6ae591fdcaf5bad4
Aethera BioSync <avatariamfoundation@gmail.com>
2025-12-22 00:05:56 +0200

    Update MedicalData.js

--- models/MedicalData.js
+++ models/MedicalData.js
@@ -1,1 +1,25 @@
+class MedicalData {
+  constructor(patientId, symptoms, predictions) {
+    this.patientId = patientId;
+    this.symptoms = symptoms;
+    this.predictions = predictions || [];
+    this.encrypted = false;  // Flag for encryption
+  }
 
+  encrypt(key) {
+    // Basic encryption placeholder (use crypto-js for real impl)
+    this.encrypted = true;
+    // Add encryption logic here later
+  }
+
+  toJSON() {
+    return {
+      patientId: this.patientId,
+      symptoms: this.symptoms,
+      predictions: this.predictions,
+      encrypted: this.encrypted,
+    };
+  }
+}
+
+module.exports = MedicalData;
