27cc53326c6bb8547730263eacd03cfeb2c72bba
Aethera BioSync <avatariamfoundation@gmail.com>
2025-12-22 00:22:25 +0200

    Update basics.js

--- test/basics.js
+++ test/basics.js
@@ -1,1 +1,10 @@
+const { expect } = require("chai");
 
+describe("MedIntel Contracts", function () {
+  it("Should deploy successfully", async function () {
+    const MedIntel = await ethers.getContractFactory("MedIntel");
+    const medIntel = await MedIntel.deploy();
+    await medIntel.deployed();
+    expect(medIntel.address).to.not.be.null;
+  });
+});
